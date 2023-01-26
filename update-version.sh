04539d4845658f8db7f60a28514064066981c466
tulikaaab <122949247+tulikaaab@users.noreply.github.com>
2023-01-25 16:26:44 -0800

    Update update-version.sh

--- update-version.sh
+++ update-version.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/bash
+#!/bin/bash
+ 
+CURRENTDATE=`date +"%Y-%m-%d %T"`
+CURRENTDATEONLY=`date +"%b %d, %Y"`
+CURRENTEPOCTIME=`date +"%Y-%m-%d %T"`
+ 
+echo Current Date is: ${CURRENTDATEONLY}
+echo Current Date and Time is: `date +"%Y-%m-%d %T"`
+echo Current Date and Time is: ${CURRENTDATE}
+echo Current Unix epoch time is: ${CURRENTEPOCTIME}
 
-
-currentDate = 'date'
-echo currentDate
+#currentDate = 'date'
+#echo currentDate
 
