847debcec0da385056d8e46765fc679b96773f1b
tulika <tbasu1@uci.edu>
2023-02-08 17:00:08 -0800

    m

---
+++ SignalDetection.py
@@ -0,0 +1,21 @@
+import unittest
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+
+
+class SignalDetection: 
+    def __init__ (self, hits, misses, falseAlarms, correctRejections):
+        self.hits = hits
+        self.misses = misses 
+        self.falseAlarms = falseAlarms
+        self.correctRejections = correctRejections 
+    def hitrate(self):
+        return self.hits/(self.hits + self.misses)
+    def farate(self):
+        return self.falseAlarms/(self.falseAlarms+self.correctRejections)
+    def d_prime(self):
+        return scipy.stats.norm.ppf(self.hitrate()) - scipy.stats.norm.ppf(self.farate())
+    def criterion(self):
+        return  -0.5 * (scipy.stats.norm.ppf(self.hitrate())+ scipy.stats.norm.ppf(self.farate()))
+    pass
